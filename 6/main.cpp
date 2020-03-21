2c2d07aa8104136ed1aed8e7c787938214fccef3
Gabriel Alves <gabrieltots@gmail.com>
2020-03-21 10:37:19 -0300

    6 - main.cpp resolvida

--- 6/main.cpp
+++ 6/main.cpp
@@ -5,33 +5,33 @@
 
 using namespace std;
 
-int getLucro(int N, int *receita, int custo) {
-    int MAX, MAX_TEMP = 0;
-    for(int i=0;i<N;i++) {
-        int lucro = receita[i] - custo;
-
-        MAX_TEMP = max(0, MAX_TEMP + lucro);
-        MAX = max(MAX, MAX_TEMP);
-    }
-
-    return MAX;
-}
-
 int main() {
     int N, custoPorDia;
 
     // value by day
+    // while(!cin.eof()) {
     while(scanf("%d %d", &N, &custoPorDia) != EOF) {
-        int *receita;
-        receita = (int*) malloc(N*sizeof(int));
+        // cin >> N >> custoPorDia;
 
+        int receita[N];
         for(int i=0;i<N;i++) {
-            scanf("%d", &receita[i]);
+            cin >> receita[i];
+            // cout << receita[i] << endl;
         }
 
-        int MAX = getLucro(N, receita, custoPorDia);
+        int MAX = 0;
+        int MAX_TEMP = 0;
+        for(int i = 0; i < N; i++) {
+            int lucro = receita[i] - custoPorDia;
+
+            MAX_TEMP = max(0, MAX_TEMP + lucro);
+            // cout << "MAX_TEMP: " << MAX_TEMP << endl;
+            MAX = max(MAX, MAX_TEMP);
+            // cout << "MAX: " << MAX << endl;
+        }
+
         cout << MAX << endl;
-        free(receita);
+        // cout << "MAX: " << MAX << endl;
     }
 
     return 0;
