3a80cbeb6d3da17787113e528cb04f0668f064f2
Gabriel Alves <gabrieltots@gmail.com>
2020-03-24 16:02:50 -0300

    3 - main.cpp OK

--- 3/main.cpp
+++ 3/main.cpp
@@ -3,31 +3,52 @@
 using namespace std;
 
 
+int LCS(string M, string N) {
+
+    int m = M.length()+1;
+    // cout << m << endl;
+    // for(int i = 0; i < m; ++i) {
+    //     cout << M[i];
+    // }
+
+    // cout << endl;
+
+    int n = N.length()+1;
+    // cout << n << endl;
+    // for(int i = 0; i < n; ++i) {
+    //     cout << Y[i];
+    // }
+
+    int L[m][n];
+    memset(L, 0, sizeof(L));
+    int ans = 0;
+
+    for(int i = 1; i < m; ++i) {
+        for(int j = 1; j < n; ++j) {
+            if(M[i-1] == N[j-1]) {
+                L[i][j] = L[i-1][j-1] + 1;
+                ans = max(ans, L[i][j]);
+            }
+        }
+    }
+    // for(int i = 0; i < m; ++i) {
+    //     for(int j = 0; j < n; ++j) {
+    //         cout << L[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    cout << ans << endl;
+}
+
+
 int main() {
 
     string s1;
     string s2;
 
     while(getline(cin, s1) && getline(cin, s2)) {
-        int i = 0;
-        int j = s1.length()-1; // remove o /n
-        int MAX = 0;
-
-        if(s1.length() > 0 && s1.length() < 51 && s2.length() > 0 && s2.length() < 51) {
-            while(s1.substr(i, j) != "") {
-                while(i < j) {
-                    string sub = s1.substr(i,j);
-                    int m = sub.length();
-                    if(s2.find(sub) != string::npos) {
-                        MAX = max(MAX, m);
-                    }
-                    i++;
-                }
-                i = 0;
-                j--;
-            }
-            cout << MAX << endl;
-        }
+        LCS(s1, s2);
     }
 
     return 0;
--- 3/main.py
+++ 3/main.py
@@ -1,38 +1,35 @@
+while True:
+    s1 = input()
+    s2 = input()
 
+    k = len(s1)
+    l = len(s2)
+    MAX = 0
+    i = 0
+    j = 0
+    it = 0
+    phrase = ''
 
-while(True):
-    while(True):
-        s1, s2 = str(input()), str(input())
-
-        k = len(s1)
-        l = len(s2)
-        MAX = 0
-        i = 0
-        j = 0
-
-        for i in range(k):
-            for j in range(l):
-                # if s2[:j] == '': continue
-                # print("{} == {}".format(s1[i], s2[:(j+1)]))
-                if s1[i] == s2[j]:
-                    # print(s1[i])
-                    # print(s2[:j])
-                    it = 1
-                    # print("{} == {}".format(s1[i:(i+it+1)], s2[:(j+it)]))
-                    # print("{} == {}".format(s1[i], s2[j]))
-                    # print("{} == {}".format(s1[i:i+it], s2[j:j+it]))
-
-                    # print("{} == {}".format(s1[i:i+it], s2[j:j+it]))
+    for i in range(k):
+        for j in range(l):
+            if s1[i] == s2[j]:
+                while s1[i] == s2[j]:
+                    # print("{} | {} == {}".format(it, s1[i], s2[j]))
+                    # print("f: {}".format(phrase))
                     # input()
-                    while s1[i:i+it] == s2[j:j+it]:
-                        it += 1
-                        # print("{} == {}".format(s1[i:i+it], s2[j:j+it]))
-                    # if(it >= 8): print("{} == {}".format(s1[i:i+it], s2[j:j+it]))
-                    MAX = max(MAX, it-1)
-                    i = it-1
-                    
-                    # MAX = max(MAX, it)
-                    break
-                # print("{} == {}".format(s1[:i],s2[:j]))
+                    i += 1
+                    j += 1
+                    it += 1
+                    # print("{} ~ {} | {} ~ {}".format(i,k,j,l))
+                    # print(it)
+                    if(i >= k or j >= l):
+                        break
 
-        print(MAX)
+                if(it != 0):
+                    # print('Entrou')
+                    MAX = max(MAX, it)
+                    it = 0
+                    break
+                break
+
+    print(MAX)
