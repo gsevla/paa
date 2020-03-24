d3c4f98c1fe02e20b0228bcf8803f30c926e77ac
Gabriel Alves <gabrieltots@gmail.com>
2020-03-24 18:14:59 -0300

    5 - main.cpp TLE

--- 5/main.cpp
+++ 5/main.cpp
@@ -2,6 +2,33 @@
 
 using namespace std;
 
+int AUX[101][101]; // valores dinamica
+
+
+int DC(vector<pair<int,int>> v, int n, int k, int r, int it) {
+    // for (int i=0; i< n; i++) { 
+    //     cout << v[i].first << " "
+    //         << v[i].second << endl; 
+    // }
+
+    if(AUX[it][k] != -1) {
+        return AUX[it][k];
+    }
+    if(it >= n || k <= 0) {
+        return AUX[it][k] = 0;
+    }
+
+    int out = DC(v, n, k, r, it+1);
+
+    if(v[it].second <= k) {
+        int in = v[it].first + DC(v, n, k-v[it].second, r, it+1);
+        return AUX[it][k] = max(in, out);
+    }
+
+    return AUX[it][k] = out;
+
+}
+
 
 int main() {
 
@@ -9,31 +36,47 @@ int main() {
     cin >> T;
 
     for(int t = 0; t < T; ++t) {
+        memset(AUX, -1, sizeof(AUX));
 
         int N;
         cin >> N;
 
         int X[N]; // poder de destruicao
         int Y[N]; // peso
-        int XY[N][2];
-        // memset(XY, -1, sizeof(XY));
+
+        vector<pair<int, int>> XY;
         for(int i = 0; i < N; ++i) {
-            cin >> XY[i][0] >> XY[i][1];
+            int x, y;
+            cin >> x >> y;
+            XY.push_back(make_pair(x, y));
         }
 
-        // for(int i = 0; i < N; ++i) {
-        //     for(int j = 0; j < 2; ++j) {
-        //         cout << XY[i][j] << " ";
-        //     }
-        //     cout << endl;
-        // }
+        // cout << endl << endl;
+
+        // sort by second value
+        sort(XY.begin(), XY.end(), [](const pair<int,int> &a, pair<int,int> &b) {return a.second < b.second;});
 
         int K; // capacidade de carga do canhao
         cin >> K;
         int R; // resistencia do castelo
         cin >> R;
 
-        // cout <<"LOADED" << endl;
+        int ans = DC(XY, N, K, R, 0);
+
+        if(ans >= R) {
+            cout << "Missao completada com sucesso" << endl;
+        } else {
+            cout << "Falha na missao" << endl;
+        }
+
+        // cout << endl << endl;
+
+        for(int i = 0; i < 50; ++i) {
+            for (int j = 0; j < 51; ++j) {
+                cout << AUX[i][j] << " ";
+            }
+            cout << endl;
+        }
 
     }
 
